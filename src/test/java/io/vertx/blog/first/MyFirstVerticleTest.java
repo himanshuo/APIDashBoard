e61383ba00e7d5eefacc9ee44ef991ba89e606f1
himanshu ojha <himanshuo@qualtrics.com>
2016-12-23 16:06:08 -0800

    CRUD operations

--- src/test/java/io/vertx/blog/first/MyFirstVerticleTest.java
+++ src/test/java/io/vertx/blog/first/MyFirstVerticleTest.java
@@ -8,6 +8,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import io.vertx.core.json.Json;
+
 
 @RunWith(VertxUnitRunner.class)
 public class MyFirstVerticleTest {
@@ -43,8 +45,9 @@ public class MyFirstVerticleTest {
 
         final Async async = context.async();
 
+        int port = MyFirstVerticle.port;
         //getNow is shortcut for get(...).end()
-        vertx.createHttpClient().getNow(8080, "localhost", "/",
+        vertx.createHttpClient().getNow(port, "localhost", "/",
                 //callback takes in response and calls response.handler with another callback with contents of response
                 response -> {
                     // body is a buffer object thus you have to call toString to read full buffer
@@ -55,4 +58,40 @@ public class MyFirstVerticleTest {
                     });
                 });
     }
+
+  @Test
+  public void checkThatTheIndexPageIsServed(TestContext context) {
+    Async async = context.async();
+    vertx.createHttpClient().getNow(MyFirstVerticle.port, "localhost", "/assets/index.html", response -> {
+      context.assertEquals(response.statusCode(), 200);
+      context.assertEquals(response.headers().get("content-type"), "text/html");
+      response.bodyHandler(body -> {
+        context.assertTrue(body.toString().contains("<title>My Whisky Collection</title>"));
+        async.complete();
+      });
+    });
+  }
+
+  @Test
+  public void checkThatWeCanAdd(TestContext context) {
+    Async async = context.async();
+    final String json = Json.encodePrettily(new Whisky("Jameson", "Ireland"));
+    final String length = Integer.toString(json.length());
+    vertx.createHttpClient().post(MyFirstVerticle.port, "localhost", "/api/whiskies")
+        .putHeader("content-type", "application/json")
+        .putHeader("content-length", length)
+        .handler(response -> {
+          context.assertEquals(response.statusCode(), 201);
+          context.assertTrue(response.headers().get("content-type").contains("application/json"));
+          response.bodyHandler(body -> {
+            final Whisky whisky = Json.decodeValue(body.toString(), Whisky.class);
+            context.assertEquals(whisky.getName(), "Jameson");
+            context.assertEquals(whisky.getOrigin(), "Ireland");
+            context.assertNotNull(whisky.getId());
+            async.complete();
+          });
+        })
+        .write(json)
+        .end();
+  }
 }
